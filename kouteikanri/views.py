1f894c642b690152e59652bc7deda56a0d50077d
mabelode96 <mbv1991sn@gmailcom>
2020-12-26 15:39:47 +0900

    v0.04

--- kouteikanri/views.py
+++ kouteikanri/views.py
@@ -166,7 +166,7 @@ class KouteiList(ListView):
             cy = 0
         else:
             cy = c_y['changey__sum']
-        progress = (py + cy) - ( pj + cj)
+        progress = (py + cy) - (pj + cj)
         ctx['progress'] = progress
         return ctx
 
