64afb3593226df01fb02817e55bb7f98fca33adf
mabelode96 <mbv1991sn@gmailcom>
2022-09-28 17:16:08 +0900

    アップロード機能修正

--- kouteikanri/views.py
+++ kouteikanri/views.py
@@ -720,7 +720,9 @@ def upload(request):
                         koutei.seisanh = ws.cell(row=j, column=7).value
                         koutei.value = koutei.value + ws.cell(row=j, column=8).value
                         koutei.seisand = koutei.seisand + ws.cell(row=j, column=9).value
-                        koutei.slicev = koutei.slicev + ws.cell(row=j, column=13).value
+                        if koutei.slicev:
+                            if ws.cell(row=j, column=13).value:
+                                koutei.slicev = koutei.slicev + ws.cell(row=j, column=13).value
                         koutei.processy = koutei.processy + ws.cell(row=j, column=16).value
                         koutei.endy = ws.cell(row=j, column=18).value
                         koutei.status = 0
