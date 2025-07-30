ae2a1039c9760b8064aa64dc2c6cda4873d3ce00
mabelode96 <mbv1991sn@gmailcom>
2025-07-30 14:15:00 +0900

    v0.16

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from .local import *
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messagesf
