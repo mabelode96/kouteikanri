a4f2ebf55d5502f01144a2ba0c7267c535842bdb
mabelode96 <mbv1991sn@gmailcom>
2025-09-03 08:45:03 +0900

    v0.18

--- serverfiles/csvimport.py
+++ serverfiles/csvimport.py
@@ -16,40 +16,42 @@ conn = psycopg2.connect(
 )
 
 for index, row in df.iterrows():
-  if row['チェーン名'] == 'ＬＷ惣菜':
-    dt = datetime.strftime(datetime.today() + timedelta(days=1), "%Y/%m/%d")
-    bn = 3
-  else:
-    dt = datetime.strftime(datetime.today(), "%Y/%m/%d")
-    bn = 2
-  if row['製造日'] == dt and row['便'] == bn:
-    # カーソルオブジェクトを作成
-    cur = conn.cursor()
-    # 区分
-    if row['処理区分名称'] == '確定':
-      kbn = "(kubun = '予測' OR kubun = '合算');"
+  # 自動更新から除外する係
+  if row['係'] != 'オートフライヤー' and  row['係'] != 'ジェットオーブンは除外しないよ！':
+    if row['チェーン名'] == 'ＬＷ惣菜':
+      dt = datetime.strftime(datetime.today() + timedelta(days=1), "%Y/%m/%d")
+      bn = 3
     else:
-      kbn = "kubun = '" + row['処理区分名称'] + "';"
-    # 開始時間
-    if row['調理開始時間'] == ''  or type(row['調理開始時間']) == float:
-      s = 'startj = Null, '
-    else:
-      s = "startj = '" + str(row['調理開始時間']) + "', "
-    # 終了時間
-    if row['加熱測定日時'] == '' or type(row['加熱測定日時']) == float:
-      if row['終了時刻'] == '' or type(row['終了時刻']) == float:
-        e = "endj = Null "
+      dt = datetime.strftime(datetime.today(), "%Y/%m/%d")
+      bn = 2
+    if row['製造日'] == dt and row['便'] == bn:
+      # カーソルオブジェクトを作成
+      cur = conn.cursor()
+      # 区分
+      if row['処理区分名称'] == '確定':
+        kbn = "(kubun = '予測' OR kubun = '合算')"
       else:
-        e = "endj = '" + str(row['終了時刻']) + "', status = 1 "
-    else:
-      e = "endj = '" + str(row['加熱測定日時']) + "', status = 1 "
-    # SQLクエリを実行
-    cur.execute("UPDATE kouteikanri_chouriproc SET " + s + e +
-                "WHERE hinban = " + str(row['品目コード']) +
-                " AND date = '" + dt + "' AND bin = " +
-                str(bn) + " AND " + kbn
-                )
-    # 変更をコミット
-    conn.commit()
-    # カーソルを閉じる
-    cur.close()
+        kbn = "kubun = '" + row['処理区分名称'] + "'"
+      # 開始時間
+      if row['調理開始時間'] == ''  or type(row['調理開始時間']) == float:
+        s = 'startj = Null, '
+      else:
+        s = "startj = '" + str(row['調理開始時間']) + "', "
+      # 終了時間
+      if row['加熱測定日時'] == '' or type(row['加熱測定日時']) == float:
+        if row['終了時刻'] == '' or type(row['終了時刻']) == float:
+          e = "endj = Null "
+        else:
+          e = "endj = '" + str(row['終了時刻']) + "', status = 1 "
+      else:
+        e = "endj = '" + str(row['加熱測定日時']) + "', status = 1 "
+      # SQLクエリを実行
+      cur.execute("UPDATE kouteikanri_chouriproc SET " + s + e +
+                  "WHERE hinban = " + str(row['品目コード']) +
+                  " AND date = '" + dt + "' AND bin = " +
+                  str(bn) + " AND " + kbn  + ";"
+                  )
+      # 変更をコミット
+      conn.commit()
+      # カーソルを閉じる
+      cur.close()
