ea55117216de6bb1a73c43e042a85f508bc3e9cd
mabelode96 <mbv1991sn@gmailcom>
2025-09-05 12:36:53 +0900

    v0.18

--- serverfiles/yobi.sh
+++ serverfiles/yobi.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-expline = " AND line != 'オートフライヤー'"
-
-sudo -u postgres psql -U postgres postgres -c "UPDATE kouteikanri_chouriproc SET startj = starty WHERE hinban < 1000 AND startj IS NULL AND starty <= CURRENT_TIMESTAMP" + expline + ";"
-sudo -u postgres psql -U postgres postgres -c "UPDATE kouteikanri_chouriproc SET endj = endy, status = 1 WHERE hinban < 1000 AND endj IS NULL AND endy <= CURRENT_TIMESTAMP" + expline + ";"
+sudo -u postgres psql -U postgres postgres -c "UPDATE kouteikanri_chouriproc SET startj = starty WHERE hinban < 1000 AND startj IS NULL AND starty <= CURRENT_TIMESTAMP;"
+sudo -u postgres psql -U postgres postgres -c "UPDATE kouteikanri_chouriproc SET endj = endy, status = 1 WHERE hinban < 1000 AND endj IS NULL AND endy <= CURRENT_TIMESTAMP;"
