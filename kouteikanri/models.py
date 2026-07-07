de8204a274558dc00734cbd46da3fc7e8f835558
mabelode96 <mbv1991sn@gmailcom>
2026-07-07 22:43:36 +0900

    v0.19

--- kouteikanri/models.py
+++ kouteikanri/models.py
@@ -15,34 +15,34 @@ class Process(models.Model):
     jigyousyo = models.CharField('事業所', blank=True, max_length=50, default=jigyousyo)
     line = models.CharField('ライン名', blank=True, max_length=50)
     period = models.CharField('時間帯', blank=True, max_length=50)
-    date = models.DateField('製造日', blank=True)
-    bin = models.IntegerField('便', blank=True)
-    hinban = models.IntegerField('品番', blank=True)
-    price = models.FloatField('単価', blank=True)
+    date = models.DateField('製造日', blank=True, null=True)
+    bin = models.IntegerField('便', blank=True, null=True)
+    hinban = models.IntegerField('品番', blank=True, null=True)
+    price = models.FloatField('単価', blank=True, null=True)
     kubun = models.CharField('区分', blank=True, max_length=10)
     name = models.CharField('製品名', blank=True, max_length=50)
-    seisanh = models.IntegerField('生産数h', blank=True)
-    value = models.IntegerField('数量', blank=True)
-    seisand = models.FloatField('生産高', blank=True)
-    conveyor = models.FloatField('コンベア速度', blank=True)
-    staff = models.IntegerField('人員', blank=True)
+    seisanh = models.IntegerField('生産数h', blank=True, null=True)
+    value = models.IntegerField('数量', blank=True, null=True)
+    seisand = models.FloatField('生産高', blank=True, null=True)
+    conveyor = models.FloatField('コンベア速度', blank=True, null=True)
+    staff = models.IntegerField('人員', blank=True, null=True)
     panmm = models.CharField('パンミリ数', blank=True, max_length=50)
-    slicev = models.IntegerField('スライス枚数', blank=True)
-    slicep = models.IntegerField('スライス能力', blank=True)
-    changey = models.IntegerField('切替予定', default=0)
-    processy = models.IntegerField('生産時間', blank=True)
-    starty = models.DateTimeField('開始予定', blank=True)
-    endy = models.DateTimeField('終了予定', blank=True)
-    startj = models.DateTimeField('開始時間', blank=True)
-    endj = models.DateTimeField('終了時間', blank=True)
-    changej = models.IntegerField('切替時間', blank=True)
-    processj = models.IntegerField('実際時間', blank=True)
-    staffj = models.IntegerField('実際人員', blank=True)
-    status = models.IntegerField('status', default=0)
-    set = models.IntegerField('set', default=0)
-    setj = models.DateTimeField('準備時間', blank=True)
+    slicev = models.IntegerField('スライス枚数', blank=True, null=True)
+    slicep = models.IntegerField('スライス能力', blank=True, null=True)
+    changey = models.IntegerField('切替予定', default=0, null=True)
+    processy = models.IntegerField('生産時間', blank=True, null=True)
+    starty = models.DateTimeField('開始予定', blank=True, null=True)
+    endy = models.DateTimeField('終了予定', blank=True, null=True)
+    startj = models.DateTimeField('開始時間', blank=True, null=True)
+    endj = models.DateTimeField('終了時間', blank=True, null=True)
+    changej = models.IntegerField('切替時間', blank=True, null=True)
+    processj = models.IntegerField('実際時間', blank=True, null=True)
+    staffj = models.IntegerField('実際人員', blank=True, null=True)
+    status = models.IntegerField('status', default=0, null=True)
+    set = models.IntegerField('set', default=0, null=True)
+    setj = models.DateTimeField('準備時間', blank=True, null=True)
     comment = models.CharField('comment', blank=True, max_length=255)
-    fkey = models.IntegerField('fkey', blank=True)
+    fkey = models.IntegerField('fkey', blank=True, null=True)
 
     tracker = FieldTracker()
 
@@ -55,20 +55,60 @@ class Process(models.Model):
 
 class Kouseihin(models.Model):
     jigyousyo = models.CharField('事業所', blank=True, max_length=50, default=jigyousyo)
-    date = models.DateField('製造日', blank=True)
-    bin = models.IntegerField('便', blank=True)
-    kubun = models.CharField('区分', blank=True, max_length=50)
-    hinban = models.IntegerField('品番', blank=True)
-    shikakaricd = models.IntegerField('構成仕掛品CD', blank=True)
-    name = models.CharField('構成仕掛品名', blank=True, max_length=50)
-    value = models.FloatField('使用量', blank=True)
-    tanni = models.CharField('単位', blank=True, max_length=50)
-    comp = models.IntegerField('完了', blank=True)
+    date = models.DateField('製造日', blank=True, null=True)
+    bin = models.IntegerField('便', blank=True, null=True)
+    kubun = models.CharField('区分', blank=True, max_length=50, null=True)
+    hinban = models.IntegerField('品番', blank=True, null=True)
+    shikakaricd = models.IntegerField('構成仕掛品CD', blank=True, null=True)
+    name = models.CharField('構成仕掛品名', blank=True, max_length=50, null=True)
+    value = models.FloatField('使用量', blank=True, null=True)
+    tanni = models.CharField('単位', blank=True, max_length=50, null=True)
+    comp = models.IntegerField('完了', blank=True, null=True)
 
     tracker = FieldTracker()
 
     class Meta:
         db_table = 'kouteikanri_kouseihin'
+
+    def __str__(self):
+        return self.name
+
+
+# 製造日, チェーン名, 便, 係, 品目コード, 品目名称, 処理区分名称, 分割区分,
+# 開始予定, 作業時間, 開始時刻, 締切予定, 終了時刻, 指示量, 実績量, 単位, 予実率,
+# 担当者名, 回数, 調理開始時間, 機械名, 調理開始担当者名, 品温測定フラグ,
+# 加熱測定日時, 加熱装置名称, 加熱温度, 時間, 塩素濃度, 浸漬時間, 担当者,
+# 冷却測定日時, 冷却装置名称, 冷却温度, 修正フラグ, みなし完了フラグ, 追加指示完了フラグ
+# ↓
+# ↓
+# 事業所, 製造日, 便, 係, 品目コード, 品目名称, 処理区分名称, 指示量, 実績量, 単位,
+# 担当者名, 品温測定フラグ, 加熱温度, 冷却温度, みなし完了フラグ, 追加指示完了フラグ
+
+
+class Jisseki(models.Model):
+    jigyousyo = models.CharField('事業所', blank=True, max_length=50, default=jigyousyo)
+    date = models.DateField('製造日', blank=True, null=True)
+    bin = models.IntegerField('便', blank=True, null=True)
+    kakari = models.CharField('係', blank=True, max_length=50, null=True)
+    kubun = models.CharField('処理区分名称', blank=True, max_length=50, null=True)
+    hinban = models.IntegerField('品番', blank=True, null=True)
+    shikakaricd = models.IntegerField('品目コード', blank=True, null=True)
+    name = models.CharField('品目名称', blank=True, max_length=50)
+    shiji = models.FloatField('指示量', blank=True, null=True)
+    value = models.FloatField('実績量', blank=True, null=True)
+    tanni = models.CharField('単位', blank=True, max_length=50, null=True)
+    tantou = models.CharField('担当者名', blank=True, max_length=50, null=True)
+    hinonflg = models.IntegerField('品温測定フラグ', blank=True, null=True)
+    kanetsu = models.CharField('加熱温度', blank=True, max_length=50, null=True)
+    reikyaku = models.CharField('冷却温度', blank=True, max_length=50, null=True)
+    minashiflg = models.IntegerField('みなし完了フラグ', blank=True, null=True)
+    tsukaflg = models.IntegerField('追加指示完了フラグ', blank=True, null=True)
+    comp = models.IntegerField('完了', blank=True, null=True)
+
+    tracker = FieldTracker()
+
+    class Meta:
+        db_table = 'kouteikanri_jisseki'
 
     def __str__(self):
         return self.name
--- serverfiles/csvcopy.py
+++ serverfiles/csvcopy.py
@@ -2,37 +2,59 @@ import os
 import shutil
 import hashlib
 import csvimport
+import deki_import
+import kousei_import
 
-n = [1, 2, 3, 4]
+n = [1, 2, 3, 4, 5, 6, 7]
 spath = ''
 lpath = ''
 
 for i in n:
-  if i == 1:
-    spath = '/mnt/nas0/生産実行システム/backup/出来高実績一覧表.csv'
-    lpath = 'kouteikanri/data/dekidaka.csv'
-  elif i == 2:
-    spath = '/mnt/nas0/生産実行システム/backup/投入実績一覧表.csv'
-    lpath = 'kouteikanri/data/tounyu.csv'
-  elif i == 3:
-    spath = '/mnt/nas0/原材料展開/csv/2303981【ＳＣＭ】ローソン２便確定/製品構成原材料データ.txt'
-    lpath = 'kouteikanri/data/set2bin.txt'
-  elif i == 4:
-    spath = '/mnt/nas0/原材料展開/csv/2303983【ＳＣＭ】ローソン３便確定/製品構成原材料データ.txt'
-    lpath = 'kouteikanri/data/set3bin.txt'
+    if i == 1:
+        spath = '/mnt/nas0/生産実行システム/backup/出来高実績一覧表.csv'
+        lpath = 'kouteikanri/data/dekidaka.csv'
+    elif i == 2:
+        spath = '/mnt/nas0/生産実行システム/backup/投入実績一覧表.csv'
+        lpath = 'kouteikanri/data/tounyu.csv'
+    elif i == 3:
+        spath = '/mnt/nas0/原材料展開/csv/2303977ローソン２便予測（全）/製品構成原材料データ.txt'
+        lpath = 'kouteikanri/data/lw2y.txt'
+    elif i == 4:
+        spath = '/mnt/nas0/原材料展開/csv/2303981【ＳＣＭ】ローソン２便確定/製品構成原材料データ.txt'
+        lpath = 'kouteikanri/data/lw2k.txt'
+    elif i == 5:
+        spath = '/mnt/nas0/原材料展開/csv/2304452ローソン３便予測/製品構成原材料データ.txt'
+        lpath = 'kouteikanri/data/lw3y.txt'
+    elif i == 6:
+        spath = '/mnt/nas0/原材料展開/csv/2303983【ＳＣＭ】ローソン３便確定/製品構成原材料データ.txt'
+        lpath = 'kouteikanri/data/lw3k.txt'
+    elif i == 7:
+        spath = '/mnt/nas0/原材料展開/csv/2304791クールデリカ１便/製品構成原材料データ.txt'
+        lpath = 'kouteikanri/data/cd1k.txt'
 
-  if os.path.isfile(spath):
-    if not os.path.isfile(lpath):
-      shutil.copy(spath, lpath)
-    else:
-      with open(lpath,'rb') as lf:
-        ldata = lf.read()
-        lhash = hashlib.md5(ldata).hexdigest()
-      with open(spath,'rb') as sf:
-        sdata = sf.read()
-        shash = hashlib.md5(sdata).hexdigest()
-      #print(lhash, shash)
-      if ldata != sdata:
-        shutil.copy(spath, lpath)
-        if i == 1:
-          csvimport.update_deki()
+    if os.path.isfile(spath):
+        if not os.path.isfile(lpath):
+            shutil.copy(spath, lpath)
+        else:
+            with open(lpath,'rb') as lf:
+                ldata = lf.read()
+                lhash = hashlib.md5(ldata).hexdigest()
+            with open(spath,'rb') as sf:
+                sdata = sf.read()
+                shash = hashlib.md5(sdata).hexdigest()
+            #print(lhash, shash)
+            if ldata != sdata:
+                shutil.copy(spath, lpath)
+                if i == 1:
+                    #csvimport.update_deki()
+                    deki_import.update_deki()
+                elif i == 3:
+                    kousei_import.update_kouseihin(3)
+                elif i == 4:
+                    kousei_import.update_kouseihin(4)
+                elif i == 5:
+                    kousei_import.update_kouseihin(5)
+                elif i == 6:
+                    kousei_import.update_kouseihin(6)
+                elif i == 7:
+                    kousei_import.update_kouseihin(7)
